a5e9b323e97ceb719c4e21ca6989ccadd048fceb
Valmir Almeida <almeidavalmir76@gmail.com>
2021-07-31 16:55:24 -0300

    :package: calculo de orcamento

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
-    var sidebar = document.querySelector(".container").classList.toggle("show-menu")
-})
+    var sidebar = document
+        .querySelector(".container")
+        .classList.toggle("show-menu");
+});
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco);
+document.querySelector("#js").addEventListener("change", atualizarPreco);
+document
+    .querySelector("#layout-sim")
+    .addEventListener("change", atualizarPreco);
+document
+    .querySelector("#layout-nao")
+    .addEventListener("change", atualizarPreco);
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value;
+    document.querySelector(
+        "label[for=prazo]"
+    ).innerHTML = `Prazo: ${prazo} semanas`;
+    atualizarPreco();
+});
+
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value;
+    const temJS = document.querySelector("#js").checked;
+    const incluiLayout = document.querySelector("#layout-sim").checked;
+    const prazo = document.querySelector("#prazo").value;
+
+    let preco = qtde * 100;
+
+    if (temJS) {
+        preco *= 1.1; //OU (preco * 0.1) OU (preco * 10 / 100)
+    }
+
+    //ShortHand
+    if (incluiLayout) preco += 500;
+    let taxaUrgencia = 1 - prazo * 0.1
+
+    preco = preco + (preco * taxaUrgencia)
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
+}
